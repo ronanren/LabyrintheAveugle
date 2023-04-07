ca5d313683d9dc1864ceb92f50b9adeec74837c0
malo.louboutin <malo.louboutin@gmail.com>
2023-04-07 15:23:21 +0200

    add pov movement

--- projet/Constants.h
+++ projet/Constants.h
@@ -2,4 +2,4 @@ const int largeur = 4;
 const int hauteur = 4;
 const float largeur_cube = 1.0;
 const float vitesse_marche = 0.15;
-const float degre_rotation_tete = 3.0;
+const float degre_rotation_tete = 90.0;
